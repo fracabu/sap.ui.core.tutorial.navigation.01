d541c9a8397062d23197e49905b03b7a882da40e
fracabu <100199965+fracabu@users.noreply.github.com>
2023-05-11 22:56:40 +0200

    fix some bugs

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,14 +1,60 @@
 sap.ui.define([
 	"sap/ui/demo/nav/controller/BaseController",
-	"sap/ui/core/Control",
-	"sap/m/Button",
-	"sap/m/MessageToast"
-], function (BaseController, MessageToast, Control, Button) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast",
+	"sap/m/Button"
+], function (BaseController, JSONModel, MessageToast, Button) {
 	"use strict";
 
-
-
 	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
+
+		onInit: function () {
+			var oViewModel = new JSONModel({
+				recipient: {
+					name: "World",
+					description: "",
+					status: "Open",
+					priority: "High"
+				}
+			});
+			this.getView().setModel(oViewModel, "view");
+		},
+
+		onPressAddNewService: function () {
+			this.byId("newServiceDialog").open();
+		},
+
+		onPressSaveNewService: function () {
+			const oModel = this.getView().getModel("mockdata");
+			oModel.refresh();
+			const oNewService = oModel.getData();
+			const oServices = oModel.getProperty("/services");
+			const iNewServiceId = oServices.length + 1; // calcola l'id del nuovo servizio
+
+			// Add the new service to the "services" array
+			oServices.push({
+				id: iNewServiceId,
+				name: oNewService.name,
+				description: oNewService.description,
+				status: oNewService.status
+			});
+
+			// Update the model and close the dialog
+			oModel.setProperty("/services", oServices);
+			this.byId("newServiceDialog").close();
+
+			// Navigate to the custom list item page
+			const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("customListItem", { serviceId: iNewServiceId });
+		},
+
+		// Function to handle the "Annulla" button press
+		onPressCancelNewService: function () {
+			// Reset the model data and close the dialog
+			const oModel = this.getView().getModel("mockdata");
+			oModel.setData({});
+			this.byId("newServiceDialog").close();
+		},
 
 		onDisplayNotFound: function () {
 			// display the "notFound" target without changing the hash
@@ -24,48 +70,9 @@ sap.ui.define([
 		onNavToProject: function () {
 			this.getRouter().navTo("projectList");
 		},
-		onAddProjectPress: function() {
-			var oView = this.getView();
-		
-			if (!this._oDialog) {
-				this._oDialog = sap.ui.xmlfragment(oView.getId(), "my.addProjectDialog", this);
-				oView.addDependent(this._oDialog);
-			}
-		
-			this._oDialog.open();
-		},
-		
-		onCloseDialog: function() {
+
+		onCloseDialog: function () {
 			this._oDialog.close();
-		},
-		
-		onPressAddNewService: function() {
-			var oDialog = this.getView().byId("newServiceDialog");
-			oDialog.open();
-		},
-		onPressCancelNewService: function() {
-			var oDialog = this.getView().byId("newProjectDialog");
-			oDialog.close();
-		},
-		onPressSaveNewService: function() {
-			var oDialog = this.getView().byId("saveServiceDialog");
-			oDialog.close();
 		}
-		
-		
-		
-		
-
-	
-
-
-
 	});
-
-	// funzione che permetta di esplodere i panel allo scroll
-
-
-
 });
-
-
